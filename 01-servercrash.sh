45129cefe14cb7b1723682a5017e5d2e6dc3ad98
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-02 22:00:55 +0530

    latest commit

--- 01-servercrash.sh
+++ 01-servercrash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SERVICE="ssh"
-WEB_URL="https://hooks.slack.com/services/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"
+WEB_URL="https://hooks.slack.com/services/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"
 
 #checking whether service is active or not
 
