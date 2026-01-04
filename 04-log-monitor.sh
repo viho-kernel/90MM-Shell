888c02772798ca53b0ef33adeabba1f37eae1d5f
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-04 22:48:54 +0530

    latest commit

---
+++ 04-log-monitor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#shebang
+
+#sys logs files are saved here
+LOG_FILE="/var/log/syslog"
+KEYWORDS="error|fail|critical|panic|success"
+
+WEB_URL="https://hooks.slack.com/services/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"
+
+HOST=$(hostname)
+DATE=$(date)
+
+MATCHES=$(grep -iE "$KEYWORDS" $LOG_FILE | tail -5)
+
+if [ ! -z "$MATCHES" ]; then #here z means length of the string is empty ! -z which means if lenght of the string is not empty.
+
+  MESSAGE="🚨 Log Alert on $HOST
+File: $LOG_FILE
+Time: $DATE
+Last Errors:
+$MATCHES"
+
+curl -s -X POST -H 'Content-type: application/json' \
+    --data "{\"text\":\"$MESSAGE\"}" $WEB_URL
+fi
