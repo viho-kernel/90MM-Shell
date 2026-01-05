4a4ebc31653a160ed38866b2622475cd5321917b
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-05 22:37:02 +0530

    latest commit

--- 05-Script-2.sh
+++ 05-Script-2.sh
@@ -7,8 +7,8 @@ DATE=$(date)
 HOST=$(hostname)
 STATUS="OK"
 
-CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}' | cut -d. -f1) 
-MEM_USAGE=$(free | awk 'NR==2 {print $3/$2 * 100}' | cut -d. -f1)
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}' | cut -d. -f1) 
+MEM=$(free | awk 'NR==2 {print $3/$2 * 100}' | cut -d. -f1)
 DISK=$(df / | awk 'NR==2 {print $5}' | cut -d% -f1)
 
 echo "System Health Report - $DATE" > $REPORT
@@ -17,13 +17,12 @@ echo "CPU Usage: $CPU%" >> $REPORT
 echo "Memory Usage: $MEM%" >> $REPORT
 echo "Disk Usage: $DISK%" >> $REPORT
 
-if [ "$CPU_USAGE" -ge 10 ] || [ "$MEM_USAGE" -ge 15 ] || [ "$DISK" -ge 12 ]; then
+if [ "$CPU" -ge 10 ] || [ "$MEM" -ge 5 ] || [ "$DISK" -ge 12 ]; then
   STATUS="NOT_OK"
 fi
 
 if [ "$STATUS" = "NOT_OK" ]; then
   
-  curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"🚨 System Health ALERT on $HOST\n$(cat $REPORT)\"}" \ $WEB_URL
+  curl -X POST -sL -H 'Content-type: application/json' --data "{\"text\":\"🚨 System Health ALERT on $HOST\n$(cat $REPORT)\"}" $WEB_URL
 
 fi
