6c572f7c0236144bebba5030fc4e300cb3ec9b3e
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-05 22:22:26 +0530

    latest commit

--- 05-Script-1.sh
+++ 05-Script-1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+#tells linux which interpreter should run this script.
 
-#Take backup's and verify
 
-SOURCE_DIR="/etc"
-BACKUP_DIR="/backup"
-LOG_FILE="/var/log/backup.log"
-DATE=$(date +"%Y-%m-%d_%H-%M")
-BACKUP_FILE="$BACKUP_DIR/etc-backup-$DATE.tar.gz"
+SOURCE_DIR="/etc" #This is the data we want to backup it contains service configs, users and sudo rules
+BACKUP_DIR="/backup" #central place to store all the backups
+LOG_FILE="/var/log/backup.log" #logs
+DATE=$(date +"%Y-%m-%d_%H-%M") #adding timestamp
+BACKUP_FILE="$BACKUP_DIR/etc-backup-$DATE.tar.gz" #full backup file name with source and time
 
 mkdir -p $BACKUP_DIR
 
 echo "[$(date)] Backup started" >> $LOG_FILE
 
-tar -czf $BACKUP_FILE $SOURCE_DIR 2>> $LOG_FILE
+tar -czf $BACKUP_FILE $SOURCE_DIR 2>> $LOG_FILE # -czf -c create archive/ -z gzip compression/ -f filename 
 
 if [ $? -ne 0 ]; then
    echo "[$(date)] Backup FAILED" >> $LOG_FILE
@@ -20,7 +20,7 @@ if [ $? -ne 0 ]; then
 
 fi 
 
-tar -tzf $BACKUP_FILE > /dev/null 2>&1
+tar -tzf $BACKUP_FILE > /dev/null 2>&1 # -tzf -t list archive contents 
 
 if [ $? -eq 0 ]; then
    echo "[$(date)] Backup SUCCESS & VERIFIED: $BACKUP_FILE" >> $LOG_FILE
