ac80055572f4bb3d9141f09432a1a81fb397d6c8
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-05 22:38:17 +0530

    latest commit

--- 05-Script-2.sh
+++ 05-Script-2.sh
@@ -22,7 +22,6 @@ if [ "$CPU" -ge 10 ] || [ "$MEM" -ge 5 ] || [ "$DISK" -ge 12 ]; then
 fi
 
 if [ "$STATUS" = "NOT_OK" ]; then
-  
   curl -X POST -sL -H 'Content-type: application/json' --data "{\"text\":\"🚨 System Health ALERT on $HOST\n$(cat $REPORT)\"}" $WEB_URL
 
 fi
