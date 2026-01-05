efd2f659124f7b11281028445d25ec9b5ba10da4
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-05 22:09:54 +0530

    latest commit

--- 05-Script-1.sh
+++ 05-Script-1.sh
@@ -5,7 +5,7 @@
 SOURCE_DIR="/etc"
 BACKUP_DIR="/backup"
 LOG_FILE="/var/log/backup.log"
-DATE=$(date + "%Y-%m-%d_%H-%M")
+DATE=$(date +"%Y-%m-%d_%H-%M")
 BACKUP_FILE="$BACKUP_DIR/etc-backup-$DATE.tar.gz"
 
 mkdir -p $BACKUP_DIR
@@ -17,7 +17,7 @@ echo "[$(date)] Backup started" >> $LOG_FILE
 tar -czf $BACKUP_FILE $SOURCE_DIR 2>> $LOG_FILE
 
 if [ $? -ne 0 ]; then
-   echo "[$(date)] Backup Failed" >> $LOG_FILE
+   echo "[$(date)] Backup FAILED" >> $LOG_FILE
    exit 1
 
 fi 
