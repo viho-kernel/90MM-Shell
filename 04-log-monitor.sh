15e17e411a5763fb01809e77444a5352c1c9fe8e
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-09 08:51:06 +0530

    latest commit

--- 04-log-monitor.sh
+++ 04-log-monitor.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-#shebang
-
 set -euo pipefail
 
-
-CONFIG_FILE="$(dirname "$0")/config.env" 
-if [ -f "$CONFIG_FILE" ]; then 
-# shellcheck disable=SC1090 
-source "$CONFIG_FILE" 
-fi 
-# Fail fast if variable not set 
-if [ -z "${SLACK_WEBHOOK_URL:-}" ]; then 
-echo "Error: SLACK_WEBHOOK_URL not set. Please create config.env or export it in your environment." 
-exit 1 
+CONFIG_FILE="$(dirname "$0")/config.env"
+if [ -f "$CONFIG_FILE" ]; then
+  # shellcheck disable=SC1090
+  source "$CONFIG_FILE"
 fi
 
-#sys logs files are saved here
+# Fail fast if variable not set
+if [ -z "${SLACK_WEBHOOK_URL:-}" ]; then
+  echo "Error: SLACK_WEBHOOK_URL not set. Please create config.env or export it in your environment."
+  exit 1
+fi
+
+# Define HOST and DATE
+HOST=$(hostname)
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Sys logs file
 LOG_FILE="/var/log/syslog"
 KEYWORDS="error|fail|critical|panic|successfully"
 
-#WEB_URL="https://hooks.slack.com/services/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"
+MATCHINGWORDS=$(grep -iE "$KEYWORDS" "$LOG_FILE" | tail -5)
 
-MATCHINGWORDS=$(grep -iE "$KEYWORDS" $LOG_FILE | tail -5)
-
-if [ ! -z "$MATCHINGWORDS" ]; then
-
+if [ -n "$MATCHINGWORDS" ]; then
    MESSAGE="🚨 Log Alert on $HOST
 File: $LOG_FILE
 Time: $DATE
-Last Errors: $MATCHINGWORDS
+Last Errors:
+$MATCHINGWORDS"
 
-  curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"$MESSAGE\"}" $SLACK_WEBHOOK_URL "
+   curl -s -X POST -H 'Content-type: application/json' \
+     --data "{\"text\":\"$MESSAGE\"}" "$SLACK_WEBHOOK_URL"
 fi
